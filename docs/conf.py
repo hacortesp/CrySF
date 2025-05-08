05a463450526df188f67b2f97f889c7e307fd9cb
hacortesp <hacortesp@hotmail.com>
2025-05-08 12:42:35 +0200

    update logo

--- docs/conf.py
+++ docs/conf.py
@@ -5,6 +5,9 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))  # go one level up to include libraries/
 
 project = 'CrySF'
 copyright = '2024, Henry Andres Cortés'
